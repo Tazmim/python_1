e2c03865947626f6fa7f27b3b0ebbd6c5c66aac8
uk- <tazmimbondhon@gmail.com>
2020-05-03 16:12:04 +0600

    string basic function

---
+++ fast.py
@@ -0,0 +1,39 @@
+print("Hello world")
+# variables
+name = "Tazmim"
+age = 20
+avg = 40.80
+# playing with strings
+
+print("Tazmim Hossain")
+name = "Tazmim Hossain"
+# concatanation
+print(name+" is a cool person")
+
+# length
+print(len(name))
+# index
+print(name.index("H"))
+# upper and lower case
+print(name.upper())
+print(name.lower())
+# index at nth position
+print(name[10])
+# replace
+print(name.replace("Tazmim","Bondhon"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
